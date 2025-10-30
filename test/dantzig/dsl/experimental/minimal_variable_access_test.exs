fa027765889f9b1aef8bdcc2ccdc06fe6e54d6d0
Emmanuel_R8 <emmanuel.rialland@gmail.com>
2025-10-30 10:01:22 +0800

    Experimental tests: adopt model_parameters where needed; normalize assertions; skip blocked map-access test (Level 8).\n- Diet Progressive: added model_parameters for food and limits data; use map_size for constraint counts\n- Variable Access Arithmetic: normalized Level 8 and temporarily skipped due to evaluator limitations\n- Minimal/Comprehensive/Simple Generator: begin model_parameters adoption

--- test/dantzig/dsl/experimental/minimal_variable_access_test.exs
+++ test/dantzig/dsl/experimental/minimal_variable_access_test.exs
@@ -5,7 +5,7 @@ defmodule Dantzig.DSL.MinimalVariableAccessTest do
   test "Minimal variable access without arithmetic" do
     food_names = ["bread"]
     
-    problem = Problem.define do
+    problem = Problem.define model_parameters: %{food_names: food_names} do
       new(name: "Minimal Test", description: "Test basic variable access")
       variables("qty", [food <- food_names], :continuous, "Amount of food")
       objective(sum(for food <- food_names, do: qty(food)), direction: :minimize)
@@ -18,7 +18,7 @@ defmodule Dantzig.DSL.MinimalVariableAccessTest do
   test "Minimal variable access with constant multiplication" do
     food_names = ["bread"]
     
-    problem = Problem.define do
+    problem = Problem.define model_parameters: %{food_names: food_names} do
       new(name: "Minimal Test", description: "Test variable * constant")
       variables("qty", [food <- food_names], :continuous, "Amount of food")
       objective(sum(for food <- food_names, do: qty(food) * 1.0), direction: :minimize)
--- test/dantzig/dsl/experimental/simple_generator_test.exs
+++ test/dantzig/dsl/experimental/simple_generator_test.exs
@@ -11,7 +11,7 @@ defmodule Dantzig.DSL.SimpleGeneratorTest do
     food_names = ["bread", "milk"]
 
     problem =
-      Problem.define do
+      Problem.define model_parameters: %{food_names: food_names} do
         new(name: "Simple Test", description: "Test generator syntax")
         variables("qty", [food <- food_names], :continuous, "Amount of food")
       end
@@ -26,7 +26,7 @@ defmodule Dantzig.DSL.SimpleGeneratorTest do
     food_names = ["bread", "milk"]
 
     problem =
-      Problem.define do
+      Problem.define model_parameters: %{food_names: food_names} do
         new(name: "Simple Test", description: "Test generator with objective")
         variables("qty", [food <- food_names], :continuous, "Amount of food")
         objective(sum(qty(:_)), :minimize)
